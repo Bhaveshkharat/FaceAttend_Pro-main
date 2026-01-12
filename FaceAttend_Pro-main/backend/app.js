b2c2f766751b5cf14bafaf8830379a4f0742b725
Bhaveshkharat <bhavesh.kharat.ds@ghrietn.raisoni.net>
2026-01-12 17:53:57 +0530

    changes1

--- FaceAttend_Pro-main/backend/app.js
+++ FaceAttend_Pro-main/backend/app.js
@@ -19,7 +19,10 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
